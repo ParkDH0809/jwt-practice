64e59918c728bef52490059187d1a36a037ccf95
박동환 <znxn123@naver.com>
2024-07-19 16:32:12 +0900

    feat: UserRepository 구현

---
+++ src/main/java/com/example/jwt_practice/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.example.jwt_practice.repository;
+
+import com.example.jwt_practice.user.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+    Optional<UserEntity> findByUsername(String username);
+
+}
